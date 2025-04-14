1d91ce45578e514cff15adbb3374a1e2401b7522
Konstantin Läufer <laufer@cs.luc.edu>
2025-04-14 16:11:29 -0500

    improved GitHub action
    Signed-off-by: Konstantin Läufer <laufer@cs.luc.edu>

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -1,1 +1,1 @@
-java -cp /Users/laufer/.vscode/extensions/tlaplus.vscode-ide-2025.3.292024/tools/tla2tools.jar tla2sany.SANY Concurrent.tla ConcurrentMultiple.tla ConcurrentMultiplePluscal.tla
+java -cp tla2tools.jar tla2sany.SANY Concurrent.tla ConcurrentMultiple.tla ConcurrentMultiplePluscal.tla
